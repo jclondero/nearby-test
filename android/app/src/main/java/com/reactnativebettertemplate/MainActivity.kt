73f47ea11f1a7568e63e083e94ebcc789125383d
Jean Carlos Londero <jeanlondero@alunos.utfpr.edu.br>
2022-09-19 19:33:46 -0300

    init commit

---
+++ android/app/src/main/java/com/reactnativebettertemplate/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.reactnativebettertemplate
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "ReactNativeBetterTemplate"
+    }
+}
